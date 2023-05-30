5102035bac86c06d17d452fe58caf81ca357c551
FanX0 <faridazhari46@gmail.com>
2023-05-31 02:55:23 +0700

    setup vite

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return inertia('Home', [
+            'user' => [
+                'name' => 'Farid',
+                'username' => 'FanX'
+            ],
+            'app_name' => config('app.name'),
+        ]);
+    }
+}
